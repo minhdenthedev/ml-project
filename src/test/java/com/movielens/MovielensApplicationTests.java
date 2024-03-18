3bbcfbe48101d91f91c34c21cc777750a82a1980
minhden <minhden.dev@gmail.com>
2024-03-18 17:02:12 +0700

    First commit

---
+++ src/test/java/com/movielens/MovielensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movielens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
